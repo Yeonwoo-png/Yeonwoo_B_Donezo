c5688ac30823709740ad8abccdacd1fa9a4bd0a5
Yeonwoo <yeonwoobae2003@gmail.com>
2025-07-14 05:12:52 -0700

    finished the getting started and backend portion of the donezo assignment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
+import router from "./routes/todo.js";
+import verifyToken from "./middleware/auth.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/todos", verifyToken, router);
+
+//this is for step 9 of backend
+//app.use("/api/todos", router);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
